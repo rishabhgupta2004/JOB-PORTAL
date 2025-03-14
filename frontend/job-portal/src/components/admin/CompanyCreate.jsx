b769ce06a6de03a9acf6218848962f57b8d6b999
RISHABH GUPTA <rishabh78000gupta@gmail.com>
2025-03-14 21:05:28 +0530

    LATEST

--- frontend/job-portal/src/components/admin/CompanyCreate.jsx
+++ frontend/job-portal/src/components/admin/CompanyCreate.jsx
@@ -16,7 +16,7 @@ const CompanyCreate = () => {
     const dispatch = useDispatch();
     const registerNewCompany = async () => {
         try {
-            const res = await axios.post("https://job-portal-8ku4.onrender.com/register", {companyName}, {
+            const res = await axios.post('https://job-portal-8ku4.onrender.com/register', {companyName}, {
                 headers:{
                     'Content-Type':'application/json'
                 },
