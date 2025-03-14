21486b3a75144a07948a3a9614e344c6c61b9bcd
RISHABH GUPTA <rishabh78000gupta@gmail.com>
2025-03-14 20:43:45 +0530

    chnages

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 // CORS Configuration
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://job-portal-8ku4.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOptions));
